1f796df2675ceaeb03bfcbf845e404836196cda7
Lesly Soveyda Solis Rosario <soveydasolis@gmail.com>
2025-11-15 19:36:56 -0500

    bloquear.php

--- bloquear.php
+++ bloquear.php
@@ -2,7 +2,7 @@
 session_start();
 include 'db.php';
 
-// Solo admin
+
 if (!isset($_SESSION['admin'])) {
     header("Location: admin.php");
     exit;
@@ -10,9 +10,10 @@ if (!isset($_SESSION['admin'])) {
 
 $uid = (int)($_GET['uid'] ?? 0);
 
-// Marcar como bloqueado (campo nuevo en usuarios)
+
 $pdo->prepare("UPDATE usuarios SET bloqueado = 1 WHERE id = ?")->execute([$uid]);
 
 header("Location: admin.php?bloqueado=1");
 exit;
+
 ?>
