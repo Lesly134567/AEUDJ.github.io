8dff2dbb09bd191ef23abb62f18eb06f65936513
Lesly Soveyda Solis Rosario <soveydasolis@gmail.com>
2025-11-15 19:43:07 -0500

    conexion.php

--- conexion.php
+++ conexion.php
@@ -1,23 +1,24 @@
 <?php
-$host = 'localhost'; // El servidor de la base de datos
-$usuario = 'root';   // El usuario por defecto de Laragon/XAMPP
-$contrasena = 'soveyda';    // La contraseña por defecto es vacía (¡NO usar en producción!)
-$base_de_datos = 'aeudj'; // Reemplaza con el nombre de tu base de datos
+$host = 'localhost'; 
+$usuario = 'root';   
+$contrasena = 'soveyda';    
+$base_de_datos = 'aeudj'; 
 
 try {
-    // 1. Crear el objeto de conexión PDO (DSN)
+   
     $conexion = new PDO("mysql:host=$host;dbname=$base_de_datos;charset=utf8", $usuario, $contrasena);
     
-    // 2. Establecer atributos para manejar errores (recomendado)
+    
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     echo "Conexión a la base de datos exitosa.";
 
 } catch (PDOException $e) {
-    // 3. Mostrar el error si la conexión falla
+  
     echo "Error de conexión: " . $e->getMessage();
-    exit(); // Detiene la ejecución si hay un error
+    exit(); 
 }
 
-// Ahora puedes usar la variable $conexion para hacer consultas SQL.
+//  $conexion para hacer consultas SQL. 
+
 ?>
